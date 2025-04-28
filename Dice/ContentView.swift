4bd116da96593507e74bbd71061b0904ccea391e
xy7-ux <ostrowskiszkola8@gmail.com>
2025-04-28 14:53:11 +0200

    Initial Commit

---
+++ Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dice
+//
+//  Created by Patryk Ostrowski on 28/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Patryk Ostrowski on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
